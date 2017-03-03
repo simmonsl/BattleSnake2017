3aef7adddf83e48d4ffd5d198961e711e6ecb56e
Jeff Manke <jeffman879@yahoo.ca>
2017-03-03 04:33:58 -0800

    adding test

--- battlesnake-python-master/app/main.py
+++ battlesnake-python-master/app/main.py
@@ -32,14 +32,14 @@ def start():
     }
 
 def init(data):
-    grid = [[0 for row in xrange(data['width'])] for col in xrange(data['height'])]
+    grid = [[0 for col in xrange(data['height'])] for row in xrange(data['width'])]
 
     for snake in data['snakes']:
         if snake['id'] == data['you']:
             mysnake = snake
         for coord in snake['coords']:
             grid[coord[0]][coord[1]] = SNAKE_BODY
-        grid[snake['coords'][0]] = SNAKE_HEAD
+        #grid[snake['coords'][0]] = SNAKE_HEAD
 
     for f in data['food']:
         grid[f[0]][f[1]] = FOOD
@@ -48,21 +48,26 @@ def init(data):
 
 
 def get_available_moves(data, grid, mysnake):
-    directions = ['up', 'down', 'left', 'right']
-    head = mysnake[0]
-    up = [head[0], head[1]+1]
-    down = [head[0], head[1]-1]
-    left = [head[0]-1, head[1]]
-    right = [head[0]+1, head[1]]
+    directions = []
 
-    if (up[1] < 0 or up[1] > data['height']) and grid[up] > 1:
-        directions.pop(0)
-    if (down[1] < 0 or down[1] > data['height']) and grid[down] > 1:
-        directions.pop(1)
-    if (left[0] < 0 or left[0] > data['width']) and grid[left] > 1:
-        directions.pop(2)
-    if (right[0] < 0 or right[0] > data['width']) and grid[right] > 1:
-        directions.pop(3)
+    head = mysnake['coords'][0]
+    up = [head[0], head[1] + 1]
+    down = [head[0], head[1] - 1]
+    left = [head[0] - 1, head[1]]
+    right = [head[0] + 1, head[1]]
+
+    if not (up[1] < 0 or up[1] > data['height'] or grid[up[0]][up[1]] > 1):
+        directions.append('up')
+        #directions.remove(directions.index('up'))
+    if not (down[1] < 0 or down[1] > data['height'] or grid[down[0]][down[1]] > 1):
+        directions.append('down')
+       # directions.remove(directions.index('down'))
+    if not (left[0] < 0 or left[0] > data['width'] or grid[left[0]][left[1]] > 1):
+        directions.append('left')
+       # directions.remove(directions.index('left'))
+    if not (right[0] < 0 or right[0] > data['width'] or grid[right[0]][right[1]] > 1):
+        directions.append('right')
+      #  directions.remove(directions.index('right'))
 
     return directions
 
@@ -71,7 +76,6 @@ def move():
     data = bottle.request.json
     # TODO: Do things with data
     mysnake, grid = init(data)
-
     directions = get_available_moves(data, grid, mysnake)
 
     return {
